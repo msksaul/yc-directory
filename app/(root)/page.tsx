504693aa8ba01e64ae7da900834a70e9a8d7df13
Melvin Calel <msksaul@gmail.com>
2025-05-16 13:48:18 -0600

    Startup Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import SearchForm from '../components/SearchForm';
+import StartupCard from '../components/StartupCard';
 
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }>}) {
 
   const query = (await searchParams).query
+
+  const posts = [{}]
 
   return (
     <>
@@ -15,6 +18,20 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         </p>
 
         <SearchForm query={query}/>
+      </section>
+
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className='mt-7 card_grid'>
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ): <p className='no results'>No startups found</p>}
+        </ul>
       </section>
     </>
   );
