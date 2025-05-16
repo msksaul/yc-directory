da487bb2afb7d24d98561c074f472a1f101986ff
Melvin Calel <msksaul@gmail.com>
2025-05-16 16:23:38 -0600

    Live API

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,14 @@
-import { client } from '@/sanity/lib/client';
 import SearchForm from '../components/SearchForm';
 import StartupCard, { StartupCardType } from '../components/StartupCard';
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
+import { sanityFetch, SanityLive } from '@/sanity/lib/live';
 
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }>}) {
 
   const query = (await searchParams).query
 
-  const posts = await client.fetch(STARTUPS_QUERY)
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY})
 
   return (
     <>
@@ -35,6 +35,8 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
           ): <p className='no results'>No startups found</p>}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
