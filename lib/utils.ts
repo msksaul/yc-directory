e51701f550be6ecaa9a7c883094d67b432f951a2
Melvin Calel <msksaul@gmail.com>
2025-05-19 12:13:37 -0600

    Startup Details

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: string) {
-  return new Date(date).toLocaleDateString('en_US', {
+  return new Date(date).toLocaleDateString('en-US', {
     month: 'long',
     day: 'numeric',
     year: 'numeric'
