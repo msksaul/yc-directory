c344cea30b2f79846e5dbbcd34b0bd702c77276b
Melvin Calel <msksaul@gmail.com>
2025-05-19 16:11:05 -0600

    Startup Form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from 'next/font/local'
 import "./globals.css";
+import { Toaster } from '@/components/ui/toaster';
 //import 'easymde/dist/easymde.min.css'
 
 const workSans = localFont({
@@ -70,6 +71,7 @@ export default function RootLayout({
         className={workSans.variable}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
